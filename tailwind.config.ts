a37dcb9c508c667dbee52c07499023ac8fafc710
nolliebigspin <alec96.w@gmail.com>
2023-10-20 17:22:28 +0200

    chore(tailwind): hover only when supported

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,9 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
   theme: {
     colors: {
       primary: "#FF8E3D",
