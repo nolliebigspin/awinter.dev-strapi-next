2b0570046203dfcb3497728984117f7cc5c15987
nolliebigspin <alec96.w@gmail.com>
2023-12-30 22:34:16 +0100

    chore(setup): WIP: update deployment 19

--- backend/scripts/deploy.sh
+++ backend/scripts/deploy.sh
@@ -1,1 +1,8 @@
-echo "Hello deploy script"
+#!/bin/bash
+
+# Pull the latest image
+# docker compose --file ./docker-compose.yml pull
+
+# Recreate and start the containers
+# docker compose --file ./docker-compose.yml up -d
+echo "Hello deploy script!"
