2f1135978ff6e0aa03b5f5d699f770cb06769803
Alec Winter <alec.winter@monday-consulting.com>
2024-01-19 21:54:56 +0100

    fix(blocks): add list styling [no actions]

--- frontend/src/components/Blocks.tsx
+++ frontend/src/components/Blocks.tsx
@@ -28,22 +28,31 @@ const Blocks = ({ content, className }: BlocksProps) => {
             heading: ({ children, level }) => {
               switch (level) {
                 case 1:
-                  return <h1 className="text-4xl font-bold">{children}</h1>;
+                  return (
+                    <h1 className="mb-4 text-4xl font-bold">{children}</h1>
+                  );
                 case 2:
-                  return <h2 className="text-4xl">{children}</h2>;
+                  return <h2 className="mb-4 text-4xl">{children}</h2>;
                 case 3:
-                  return <h3 className="text-2xl font-bold">{children}</h3>;
+                  return (
+                    <h3 className="mb-2 text-2xl font-bold">{children}</h3>
+                  );
                 case 4:
-                  return <h4 className="text-2xl">{children}</h4>;
+                  return <h4 className="mb-2 text-2xl">{children}</h4>;
                 case 5:
-                  return <h5 className="text-xl font-bold">{children}</h5>;
+                  return <h5 className="mb-2 text-xl font-bold">{children}</h5>;
                 case 6:
                   return <h6 className="text-xl">{children}</h6>;
                 default:
                   return <h3 className="text-4xl font-bold">{children}</h3>;
               }
             },
-            link: ({ children, url }) => <Link href={url}>{children}</Link>,
+            link: ({ children, url }) => (
+              <Link href={url} className="italic hover:underline">
+                {children}
+              </Link>
+            ),
+            "list-item": ({ children }) => <li>• {children}</li>,
           }}
           modifiers={{
             bold: ({ children }) => (
